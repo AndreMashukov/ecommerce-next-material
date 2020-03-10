8bd0a43af69872a231a9b7899ab61e9cb89f9a8e
Andrey Mashukov <andre.mashukov@gmail.com>
2020-03-10 10:54:46 +0700

    minor changes

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const API_BASE = process.env.NODE_ENV !== 'production' 
+  ? 'http://localhost:3000/api/'
+  : 'http://localhost:3000/api/';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Home } from '../components';
-// import { Section } from '../services/Section';
 import fetch from 'isomorphic-unfetch';
+import { API_BASE } from '../constants';
 
 export default class extends React.Component {
-  // tslint:disable-next-line: no-any
   static async getInitialProps() {
-    const res = await fetch('http://localhost:3000/api/sections/block?blockId=4');
-    const json = await res.json();
-    // tslint:disable-next-line: no-console
-    console.log(json);
-    return { sections: json };
+    const res = await fetch(`${API_BASE}sections/block?blockId=4`);
+    return { sections: res.json() };
   }
 
   render() {
