3624a1fa21099d6ab74ea8fc2c20c0c6f1f421e2
Andrey Mashukov <andre.mashukov@gmail.com>
2020-03-10 11:03:15 +0700

    section model

---
+++ src/models/section.ts
@@ -0,0 +1,8 @@
+export interface Section {
+  id: number;
+  blockId: number;
+  sectionId: number;
+  name: string;
+  description: string;
+  code: string;
+}
--- src/store/SectionContext.ts
+++ src/store/SectionContext.ts
@@ -1,13 +1,5 @@
 import React from 'react';
-
-interface Section {
-    id: number;
-    blockId: number;
-    sectionId: number;
-    name: string;
-    description: string;
-    code: string;
-}
+import { Section } from '../models';
 
 export default React.createContext({ setSections: (sections: Section[]) => {
   // tslint:disable-next-line: no-console
