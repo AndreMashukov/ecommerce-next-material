d1a80907b186965573422ebdf9443cc103cd06e6
Andrey Mashukov <andre.mashukov@gmail.com>
2020-03-12 15:23:48 +0700

    minor changes

--- src/store/SectionProvider.tsx
+++ src/store/SectionProvider.tsx
@@ -21,20 +21,6 @@ export const SectionContext = createContext({
   getAll: getAllSections()
 });
 
-
-// tslint:disable-next-line: no-any
-export const SectionProvider = (props: any) => {
-  const value = {
-    getAll: getAllSections()
-  };
-
-  return (
-    <SectionContext.Provider value={value}>
-      {props.children}
-    </SectionContext.Provider>
-  );
-};
-
 export const useSections = () => {
   return useContext(SectionContext);
 };
