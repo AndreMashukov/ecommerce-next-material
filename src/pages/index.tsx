672cdf1d93c301180ce1ef8a0504acdb522f8ecb
Andrey Mashukov <andre.mashukov@gmail.com>
2020-03-14 19:07:33 +0700

    minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Home } from '../components';
 import { Section } from '../models';
-import { SectionApi } from '../services/CatalogApi';
+import { getSections } from '../services/CatalogApi';
 import { PRODUCT_CATALOG } from '../constants';
 
 interface Props {
   sections: Section[];
 }
 
-const sectionApi = SectionApi();
-
 export default class extends React.Component<Props> {
   static async getInitialProps() {
-    const sectionList: Section[] = await sectionApi(PRODUCT_CATALOG);
+    const sectionList: Section[] = await getSections(PRODUCT_CATALOG);
     return { sections: sectionList };
   }
 
