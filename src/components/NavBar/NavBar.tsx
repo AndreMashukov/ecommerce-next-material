c35fe862e7a5cb5fb250276ec9aa1e73666395de
Andrey Mashukov <andre.mashukov@gmail.com>
2020-03-17 16:59:08 +0700

    NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,31 +4,57 @@ import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import HomeIcon from '@material-ui/icons/Home';
+// import HomeIcon from '@material-ui/icons/Home';
 import './NavBar.scss';
-import Link from 'next/link';
+// import Link from 'next/link';
+import { Typography } from '@material-ui/core';
 
 export const NavBar = class extends React.Component {
   render() {
     return (
       <AppBar position="static">
         <Toolbar>
-          <div className="navbar-layout">
-            <Grid container direction="row" justify="space-between" alignItems="flex-start" spacing={2}>
-              <Grid item>
-                <IconButton aria-label="home" color="inherit">
-                  <Link href={'/'} prefetch={false}>
-                    <HomeIcon />
-                  </Link>
-                </IconButton>
-              </Grid>
-              <Grid item>
-                <IconButton aria-label="home" color="inherit">
-                  <ShoppingCartIcon />
-                </IconButton>
-              </Grid>
+          <Grid container direction="column" alignItems="center" spacing={1}>
+            <Grid item>
+              <div className="navbar-layout">
+                <Grid container direction="row" justify="flex-start" spacing={2}>
+                  <Grid item>
+                    <Typography>Доставка</Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography>Оплата</Typography>
+                  </Grid>
+                </Grid>
+              </div>
             </Grid>
-          </div>
+            <Grid item>
+              <div className="bottom-section">
+                <div className="navbar-layout">
+                  <Grid container direction="row" justify="space-between" alignItems="flex-start" spacing={2}>
+                    <Grid item>
+                      <Grid container direction="row" justify="flex-start" alignItems="center" spacing={5}>
+                        <Grid item>
+                          <div className="img-molecule">
+                            <img src="/img/molecule.svg" />
+                          </div>
+                        </Grid>
+                        <Grid>
+                          <div className="img-logo">
+                            <img src="/img/logo.svg" />
+                          </div>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Grid item>
+                      <IconButton aria-label="home" color="inherit">
+                        <ShoppingCartIcon />
+                      </IconButton>
+                    </Grid>
+                  </Grid>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     );
