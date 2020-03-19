2ad69c7483eb2d5c279427a87fde3bcdd9e97b81
Andrey Mashukov <andre.mashukov@gmail.com>
2020-03-19 11:22:58 +0700

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,13 @@
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import { makeStyles } from '@material-ui/styles';
+import theme from '../../theme/theme';
+import './Footer.scss';
+import { Grid, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
-  stickToBottom: {
-    width: '100%'
+  footer: {
+    width: '100%',
+    height: '100px',
+    'background-color': theme.palette.primary.main
   }
 });
 
@@ -13,10 +15,18 @@ export const Footer = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.stickToBottom}>
-      <BottomNavigation>
-        <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-      </BottomNavigation>
+    <div className={classes.footer}>
+      <div className="footer-layout">
+        <Grid container direction="row" justify="flex-start" alignItems="center">
+          <Grid item>
+            <div className="footer-block">
+              <Typography variant="body1">
+                © Интернет-Магазин MIRRA, 2020
+              </Typography>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 };
