d1d68c15dbdf14332c961c0e2df708edbd70a778
Andrey Mashukov <andre.mashukov@gmail.com>
2020-03-31 17:10:55 +0700

    CartItem model

---
+++ src/models/CartItem.ts
@@ -0,0 +1,7 @@
+export interface CartItem {
+  fuserId: number;
+  productId: number;
+  price: number;
+  quantity: number;
+  currency: string;
+}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,3 @@
 export * from './Section';
 export * from './Product';
+export * from './CartItem';
