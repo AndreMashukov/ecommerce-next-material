dc5024c60a89fc2bceb61381a4ac66098b49c321
Andrey Mashukov <andre.mashukov@gmail.com>
2020-04-04 11:29:49 +0700

    CART_GET

--- src/store/reducers/cartReducer.ts
+++ src/store/reducers/cartReducer.ts
@@ -1,7 +1,7 @@
 import { Product, CartItem } from '../../models';
 import TYPES from './types';
 import { clearSelect, removeSelect, addSelect } from './selectors/cartSelectors';
-import { addToCart } from '../../services/CartApi';
+import { addToCart, getCart } from '../../services/CartApi';
 
 interface CartAction {
   type: TYPES;
@@ -17,18 +17,30 @@ interface CartState {
 
 export default async function cartReducer(state: CartState, action: CartAction): Promise<CartState> {
   switch (action.type) {
+    case TYPES.CART_GET:
+      return new Promise(async _resolve => {
+        getCart(1).then(resp => {
+          _resolve({
+            items: resp
+          });
+        });
+      });
     case TYPES.CART_CLEAR:
-      return new Promise( (_resolve) => _resolve( { products: clearSelect() }));
+      return new Promise(_resolve => _resolve({ products: clearSelect() }));
     case TYPES.CART_REMOVE:
-      return new Promise( (_resolve) => _resolve( {
-        products: removeSelect(state.products, action.id),
-      }));
+      return new Promise(_resolve =>
+        _resolve({
+          products: removeSelect(state.products, action.id),
+        }),
+      );
     case TYPES.CART_ADD:
       await addToCart(action.item);
-      return new Promise( (_resolve) => _resolve( {
-        products: addSelect(state.products, action.product),
-      }));
+      return new Promise(_resolve =>
+        _resolve({
+          products: addSelect(state.products, action.product),
+        }),
+      );
     default:
-      return new Promise( (_resolve) => _resolve( state));
+      return new Promise(_resolve => _resolve(state));
   }
 }
