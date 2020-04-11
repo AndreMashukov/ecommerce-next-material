f6d326b229ffc7cce5f4306933f39d083b69d95d
Andrey Mashukov <andre.mashukov@gmail.com>
2020-04-11 10:52:05 +0800

    Leveraging useState for NavBarCart

--- src/components/NavBarCart/NavBarCart.tsx
+++ src/components/NavBarCart/NavBarCart.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Popover from '@material-ui/core/Popover';
 import ListProductsCart from '../ListProductsCart/ListProductsCart';
@@ -6,6 +6,8 @@ import { makeStyles } from '@material-ui/styles';
 import theme from '../../theme/theme';
 import CartContextManager from '../../store/CartContext/CartContextManager';
 import CartContext from '../../store/CartContext/CartContext';
+import { getCart } from '../../services/CartApi';
+import { CartItem } from '../../models';
 
 const cartIsEmpty = 'корзина пуста';
 const cartIsNotEmpty = 'всего товаров';
@@ -27,10 +29,10 @@ export const NavBarCart = () => {
   const classes = useStyles();
   const divRef = React.useRef();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const { items, syncCart } = useContext<CartContextManager>(CartContext);
+  const { items } = useContext<CartContextManager>(CartContext);
 
   const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+  const id = open ? 'cart-popover' : undefined;
 
   const handlePopoverOpen = () => {
     setAnchorEl(divRef.current);
@@ -40,20 +42,27 @@ export const NavBarCart = () => {
     setAnchorEl(null);
   };
 
+  const [ cart, setCart ] = useState({ items: [] });
+
   useEffect(() => {
-    syncCart();
-  }, []);
+    const getCartItems = async () => {
+      const cartItems: CartItem[] = await getCart(1);
+      setCart({items: cartItems});
+    };
+
+    getCartItems();
+  }, [items]);
 
   return (
     <div ref={divRef}>
-      <Grid container 
+      <Grid container
         onClick={handlePopoverOpen} className={classes.cartInfo}
         direction="row"
         justify="space-between"
         alignItems="center"
         spacing={2}>
         <Grid item>
-          {items.length === 0 ? cartIsEmpty : `${cartIsNotEmpty}: ${items.length}`}
+          {cart.items.length === 0 ? cartIsEmpty : `${cartIsNotEmpty}: ${cart.items.length}`}
         </Grid>
         <Grid item>
           <img src="/img/bag_full.png" />
