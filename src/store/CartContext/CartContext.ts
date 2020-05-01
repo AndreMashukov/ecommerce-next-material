e35cc8ece17c3509e0b887d46bc824d13a24c748
Andrey Mashukov <andre.mashukov@gmail.com>
2020-05-01 15:18:14 +0800

    Removed unnecessary declarations from CartContext

--- src/store/CartContext/CartContext.ts
+++ src/store/CartContext/CartContext.ts
@@ -1,15 +1,10 @@
 import React from 'react';
-import { CartItem } from '../../models';
 import CartContextManager from './CartContextManager';
 
 const CartContext = React.createContext<CartContextManager>({
-  // tslint:disable-next-line: no-console
-  removeItem: (sessionId: string, id: number) => console.log(`${sessionId}${id}`),
-    // tslint:disable-next-line: no-console
-  addItem: (sessionId: string, item: CartItem, callback: () => void) => console.log
-    (`${sessionId}${item}${callback}`),
-  // tslint:disable-next-line: no-console
-  syncCart: (sessionId: string) => console.log(`${sessionId}`),
+  removeItem: null,
+  addItem: null,
+  syncCart: null,
   items: []
 });
 
