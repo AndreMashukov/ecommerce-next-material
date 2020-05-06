72e076667548f422707cb1520137967b67235425
Andrey Mashukov <andre.mashukov@gmail.com>
2020-05-06 10:11:50 +0800

    Refactor themes

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -7,49 +7,53 @@ import pink from '@material-ui/core/colors/pink';
 // It's optional.
 
 const colors = {
-  primaryLight: grey[50],
-  primaryMain: grey[300],
-  primaryDark: grey[800],
-  secondaryLight: teal[200],
-  secondaryMain: teal[300],
-  secondaryDark: pink[600]
+  primary: {
+    light: grey[50],
+    main: grey[300],
+    dark: grey[800]
+  },
+  secondary: {
+    light: teal[200],
+    main: teal[300],
+    dark: pink[600]
+  }
 };
 
 export default createMuiTheme({
   palette: {
     primary: {
-      light: colors.primaryLight,
-      main: colors.primaryMain,
-      dark: colors.primaryDark
+      light: colors.primary.light,
+      main: colors.primary.main,
+      dark: colors.primary.dark
     },
     secondary: {
-      light: colors.secondaryLight,
-      main: colors.secondaryMain,
-      dark: colors.secondaryDark
+      light: colors.secondary.light,
+      main: colors.secondary.main,
+      dark: colors.secondary.dark
     }
   },
   overrides: {
     MuiButton: {
       outlined: {
         fontWeight: 'bold',
-        color: colors.secondaryDark,
-        borderColor: colors.secondaryDark,
-        background: `linear-gradient(45deg, ${colors.primaryLight} 30%, ${colors.primaryLight} 90%)`,
+        color: colors.secondary.dark,
+        borderColor: colors.secondary.dark,
+        background: `linear-gradient(45deg, ${colors.primary.light} 30%, ${colors.primary.light} 90%)`,
 
         '&:hover': {
-          color: colors.primaryLight,
-          borderColor: colors.primaryLight,
-          background: `linear-gradient(45deg, ${colors.secondaryDark} 30%, ${colors.secondaryDark} 90%)`,
+          color: colors.primary.light,
+          borderColor: colors.primary.light,
+          background: `linear-gradient(45deg, ${colors.secondary.dark} 30%, ${colors.secondary.dark} 90%)`
         }
       },
       contained: {
         fontWeight: 'bold',
-        color: colors.secondaryDark,
-        background: `linear-gradient(45deg, ${colors.primaryLight} 30%, ${colors.primaryLight} 90%)`,
+        color: colors.secondary.dark,
+        background: `linear-gradient(45deg, ${colors.primary.light} 30%, ${colors.primary.light} 90%)`,
 
         '&:hover': {
-          color: colors.primaryLight,
-          background: `linear-gradient(45deg, ${colors.secondaryDark} 30%, ${colors.secondaryDark} 90%)`
+          color: colors.primary.light,
+          background: `linear-gradient(45deg, ${colors.secondary.dark} 30%, ${colors.secondary.dark} 90%)`
         }
       }
     }
