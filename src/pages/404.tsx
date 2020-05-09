5fe461e071f024b75bdd83ef1c2efd455ca68570
Andrey Mashukov <andre.mashukov@gmail.com>
2020-05-09 12:07:59 +0800

    Page404 for [section] route

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import Typography from '@material-ui/core/Typography';
+
+export default function Page404() {
+  return <Typography variant="h4" color="textPrimary">
+    СТРАНИЦА НЕ НАЙДЕНА (404 NOT FOUND)
+    </Typography>;
+}
--- src/pages/catalog/mirra-test/[section].tsx
+++ src/pages/catalog/mirra-test/[section].tsx
@@ -8,6 +8,7 @@ import { NextPageContext } from 'next';
 import { ProductList } from '../../../components/ProductList/ProductList';
 import { handleSession } from '../../../utils/handleSession';
 import ShopBreadcrumbs from '../../../components/ShopBreadcrumbs/ShopBreadcrumbs';
+import Page404 from '../../404';
 
 interface Props {
   products: Product[];
@@ -17,41 +18,76 @@ interface Props {
 }
 
 function getSectionByCode(sections: Section[], code: string): Section {
-  return sections.find(item => item.code === code);
+  return sections.find((item) => item.code === code);
 }
 
 const SectionPage = (props: Props) => {
   const { sections, currentSection } = props;
+  let sectionName;
+  try {
+    sectionName = getSectionByCode(sections, currentSection).name;
+  } catch(err) {
+    sectionName = undefined;
+  }
+
   return (
     <div>
-      <div style={{ marginBottom: '30px' }}>
-        <ShopBreadcrumbs sectionName={getSectionByCode(sections, currentSection).name}/>
-      </div>
-      <Grid direction="row" justify="space-around" alignItems="center"
-        spacing={2} container style={{marginBottom: '20px', minHeight: '100px'}}>
-        <Grid item xs={4}>
-          <Typography variant="h4" color="textPrimary">
-            {getSectionByCode(sections, currentSection).name}
-          </Typography>
-        </Grid>
-        <Grid item xs={4}>
-          <Typography variant="body2" color="textPrimary">
-            <span dangerouslySetInnerHTML={{__html:
-              `${getSectionByCode(sections, currentSection).description}`}} />
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid container direction="row" justify="space-between" alignItems="flex-start" spacing={2}>
-        <Grid item xs={12}>
-          <ProductList {...props} />
-        </Grid>
-      </Grid>
+      {sectionName ? (
+        <div>
+          <div style={{ marginBottom: '30px' }}>
+            <ShopBreadcrumbs sectionName={sectionName} />
+          </div>
+          <Grid
+            direction="row"
+            justify="space-around"
+            alignItems="center"
+            spacing={2}
+            container
+            style={{ marginBottom: '20px', minHeight: '100px' }}
+          >
+            <Grid item xs={4}>
+              <Typography variant="h4" color="textPrimary">
+                {sectionName}
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="body2" color="textPrimary">
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: `${
+                      getSectionByCode(sections, currentSection).description
+                    }`
+                  }}
+                />
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="flex-start"
+            spacing={2}
+          >
+            <Grid item xs={12}>
+              <ProductList {...props} />
+            </Grid>
+          </Grid>
+        </div>
+      ) : (
+        <div>
+          <Page404 />
+        </div>
+      )}
     </div>
   );
 };
 
 SectionPage.getInitialProps = async (ctx: NextPageContext) => {
-  const productList = await getProducts({ blockId: PRODUCT_CATALOG_ID, sectionCode: ctx.query.section });
+  const productList = await getProducts({
+    blockId: PRODUCT_CATALOG_ID,
+    sectionCode: ctx.query.section
+  });
   const sectionList = await getSections(PRODUCT_CATALOG_ID);
   const session = await handleSession(ctx);
   return {
