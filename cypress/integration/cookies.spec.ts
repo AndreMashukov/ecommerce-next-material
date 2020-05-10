9d090e8b3be698fe9daf9fa39f17dfce3f604256
Andrey Mashukov <andre.mashukov@gmail.com>
2020-05-10 15:18:06 +0800

    created model Cookie to have at least one import in cookies.spec.ts

--- cypress/integration/cookies.spec.ts
+++ cypress/integration/cookies.spec.ts
@@ -1,21 +1,21 @@
 /// <reference types="cypress" />
+import { Cookie } from '../models/Cookie';
 
 context('Cookies', () => {
   beforeEach(() => {
     Cypress.Cookies.debug(true);
-
     cy.visit('/');
     cy.clearCookies();
   });
 
   it('get a sessionId from browser cookie', () => {
+    let browserCookies: Cookie[];
     cy.visit('/');
-
-    // cy.getCookie() yields a cookie object
     cy.getCookies()
       .should('have.length', 1)
       .should((cookies) => {
-        expect(cookies[0].name).to.be.equal('sessionId');
+        browserCookies = cookies;
+        expect(browserCookies[0].name).to.be.equal('sessionId');
     });
   });
 });
---
+++ cypress/models/Cookie.ts
@@ -0,0 +1,1 @@
+export type Cookie = Partial<Cypress.Cookie>;
