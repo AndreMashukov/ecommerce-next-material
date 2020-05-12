e61638156a75780bd414d453fb132730b66085ed
Andrey Mashukov <andre.mashukov@gmail.com>
2020-05-12 08:36:03 +0800

    Added separate function to handle state change in cartReducer

--- src/store/CartContext/reducers/cartReducer.ts
+++ src/store/CartContext/reducers/cartReducer.ts
@@ -3,35 +3,52 @@ import { CartAction, CartState } from './models';
 import TYPES from './types';
 import { addToCart, getCart, removeFromCart } from '../../../services/CartApi';
 
+const cartRemoveReducer = async (
+  action: CartAction,
+  callback: (newState: CartState) => void
+) => {
+  await removeFromCart(action.sessionId, parseInt(action.id, 0));
+  const cart = await getCart(action.sessionId);
+  const updatedCart: CartItem[] = [];
+  cart.forEach((item) => {
+    if (item.productId !== parseInt(action.id, 0)) {
+      updatedCart.push(item);
+    }
+  });
+  callback({ items: updatedCart });
+};
+
+const cartAddReducer = async (
+  action: CartAction,
+  state: CartState,
+  callback: (newState: CartState) => void
+) => {
+  const result = await addToCart(action.item);
+  if (result.ok) {
+    callback({
+      ...state,
+      items: await getCart(action.sessionId),
+      httpStatus: result
+    });
+  } else {
+    callback({ ...state, httpStatus: result });
+  }
+};
+
 export default async function cartReducer(
   state: CartState,
   action: CartAction
 ): Promise<CartState> {
-  return new Promise(async _resolve => {
-    let cart: CartItem[] = [];
+  return new Promise(async (_resolve) => {
     switch (action.type) {
       case TYPES.CART_GET:
-        cart = await getCart(action.sessionId);
-        _resolve({ items: cart });
+        _resolve({ items: await getCart(action.sessionId) });
         break;
       case TYPES.CART_REMOVE:
-        await removeFromCart(action.sessionId, parseInt(action.id, 0));
-        cart = await getCart(action.sessionId);
-        const updatedCart: CartItem[] = [];
-        cart.forEach(item => {
-          if (item.productId !== parseInt(action.id, 0)) {
-            updatedCart.push(item);
-          }
-        });
-        _resolve({ items: updatedCart});
+        cartRemoveReducer(action, _resolve);
         break;
       case TYPES.CART_ADD:
-        const result = await addToCart(action.item);
-        if (result.ok) {
-          _resolve({...state, items: await getCart(action.sessionId), httpStatus: result });
-        } else {
-          _resolve({...state, httpStatus: result});
-        }
+        cartAddReducer(action, state, _resolve);
         break;
       default:
         _resolve(state);
