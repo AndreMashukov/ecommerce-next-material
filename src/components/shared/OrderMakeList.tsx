9f1f766244b2bd9fdd36983d1b92820bb80d7a64
Andrey Mashukov <andre.mashukov@gmail.com>
2020-05-25 15:18:08 +0800

    minor changes

--- src/components/shared/OrderMakeList.tsx
+++ src/components/shared/OrderMakeList.tsx
@@ -1,16 +1,16 @@
-import React, { useContext, useCallback } from 'react';
+import React, { useContext } from 'react';
 import CartContext from '../../store/CartContext/CartContext';
-import SessionContext from '../../store/SessionContext/SessionContext';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/styles';
 import theme from '../../theme/theme';
 import { CartItem } from '../../models';
+import clsx from 'clsx';
 
 const useStyles = makeStyles({
   root: {
-    backgroundColor: theme.palette.primary.main,
+    backgroundColor: theme.palette.primary.main
   },
   grid: {
     padding: '30px 70px 30px 70px',
@@ -26,26 +26,29 @@ const useStyles = makeStyles({
 
 export const OrderMakeList = () => {
   const classes = useStyles();
-  const { syncCart, items, getItems } = useContext(CartContext);
-  const { getSessionId } = useContext(SessionContext);
-
-  useCallback(() => {
-    syncCart(getSessionId());
-  }, [items]);
-
-  // tslint:disable-next-line: no-console
-  console.log(items);
+  const { getItems } = useContext(CartContext);
 
   return (
     <div className={classes.root}>
       <Grid container justify="center">
         <Grid item className={classes.grid} xs={10}>
           <Paper className={classes.paper}>
-            {getItems().map((item: CartItem) => (
-              <div key={`OrderMakeList_${item.productId}`}>
+            {getItems().map((item, index) => (
+              <div
+                key={`OrderMakeList_${item.productId}`}
+                className={clsx(index !== 0 && classes.border)}
+              >
                 <OrderMakeItem {...item} />
               </div>
             ))}
+            <div>
+              <Typography variant="caption">
+                Пожалуйста, внимательно проверяйте Ваши персональные данные при
+                регистрации и оформлении заказа. Неправильно указанный номер
+                телефона, неточный или неполный адрес могут привести к
+                дополнительной задержке.
+              </Typography>
+            </div>
           </Paper>
         </Grid>
       </Grid>
@@ -54,19 +57,27 @@ export const OrderMakeList = () => {
 };
 
 const OrderMakeItem = (item: CartItem) => {
-  // const classes = useStyles();
-
   return (
-    <Grid container direction="row" justify="space-between" alignItems="center">
+    <Grid
+      container
+      direction="row"
+      justify="space-between"
+      alignItems="center"
+      style={{ padding: '20px 0 20px 0' }}
+    >
       <Grid item xs={8}>
         <Typography>{item.name}</Typography>
         <Typography color="textSecondary">{item.skuCode}</Typography>
       </Grid>
       <Grid item xs={3}>
-        <Typography>{item.quantity} x {parseInt(item.price.toString(), 0)} ₽</Typography>
+        <Typography>
+          {item.quantity} x {parseInt(item.price.toString(), 0)} ₽
+        </Typography>
       </Grid>
       <Grid item>
-        <Typography>{item.quantity * parseInt(item.price.toString(), 0)} ₽</Typography>
+        <Typography>
+          {item.quantity * parseInt(item.price.toString(), 0)} ₽
+        </Typography>
       </Grid>
     </Grid>
   );
