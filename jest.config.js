1824e54b99bc85ed37486a5cdcc235e7615413bb
Andrey Mashukov <andre.mashukov@gmail.com>
2020-06-26 15:21:19 +0800

    Fixing tests

--- jest.config.js
+++ jest.config.js
@@ -23,5 +23,10 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
 
   // Module file extensions for importing
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.test.json"
+    }
+  }
 };
