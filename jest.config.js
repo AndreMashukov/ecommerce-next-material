42d7402d75611e71f6b7da06dbe420b01df915db
Andrey Mashukov <andre.mashukov@gmail.com>
2020-06-26 15:33:44 +0800

    Fixing test in pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.test.json"
+    }
+  },
   // The root of your source code, typically /src
   // `<rootDir>` is a token Jest substitutes
   roots: ["<rootDir>/src"],
@@ -6,6 +11,7 @@ module.exports = {
   // Jest transformations -- this adds support for TypeScript
   // using ts-jest
   transform: {
+    "^.+\\.jsx?$": "<rootDir>/node_modules/babel-jest",
     "^.+\\.tsx?$": "ts-jest"
   },
 
@@ -23,10 +29,5 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
 
   // Module file extensions for importing
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.test.json"
-    }
-  }
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
