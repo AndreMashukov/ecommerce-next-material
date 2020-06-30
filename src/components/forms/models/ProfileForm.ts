b758c19d0d277a5fc90cf8b81404236047056146
Andrey Mashukov <andre.mashukov@gmail.com>
2020-06-30 18:39:38 +0800

    refactor RegisterForm model

---
+++ src/components/forms/models/ProfileForm.ts
@@ -0,0 +1,37 @@
+import { ChangeEventType, TextField } from './GlobalModels';
+
+export interface ProfileFormFields {
+  lastName: TextField;
+  firstName: TextField;
+  password: TextField;
+  confirmPassword: TextField;
+}
+
+export interface ProfileFormHandlers{
+  onLastNameChange: (event: ChangeEventType) => TextField;
+  lastNameDirty: () => TextField;
+  onFirstNameChange: (event: ChangeEventType) => TextField;
+  firstNameDirty: () => TextField;
+  onPasswordChange: (event: ChangeEventType) => TextField;
+  clearPassword: () => TextField;
+  passwordDirty: () => TextField;
+  onConfirmPasswordChange: (event: ChangeEventType) => TextField;
+  clearConfirmPassword: () => TextField;
+  confirmPasswordDirty: () => TextField;
+}
+
+export interface ProfileFormErrors {
+  lastNameError: (lastName: TextField) => boolean;
+  firstNameError: (firstName: TextField) => boolean;
+  passwordError: (password: TextField) => boolean;
+  confirmPasswordError: (confirmPassword: TextField) => boolean;
+}
+
+interface ProfileFormSubmit {
+  profileSubmit: () => ProfileFormFields | undefined;
+}
+
+export type RegisterFormProps = ProfileFormFields
+  & ProfileFormHandlers
+  & ProfileFormErrors
+  & ProfileFormSubmit;
--- src/components/forms/models/RegisterForm.ts
+++ src/components/forms/models/RegisterForm.ts
@@ -1,38 +1,25 @@
 import { ChangeEventType, TextField } from './GlobalModels';
+import {
+  ProfileFormFields,
+  ProfileFormHandlers,
+  ProfileFormErrors
+} from './ProfileForm';
 
-export interface RegisterFormFields {
+export interface RegisterFormFields extends ProfileFormFields {
   email: TextField;
   phone: TextField;
-  lastName: TextField;
-  firstName: TextField;
-  password: TextField;
-  confirmPassword: TextField;
 }
 
-interface RegisterFormHandlers{
+interface RegisterFormHandlers extends ProfileFormHandlers {
   onEmailChange: (event: ChangeEventType) => TextField;
   emailDirty: () => TextField;
   onPhoneChange: (event: ChangeEventType) => TextField;
   phoneDirty: () => TextField;
-  onLastNameChange: (event: ChangeEventType) => TextField;
-  lastNameDirty: () => TextField;
-  onFirstNameChange: (event: ChangeEventType) => TextField;
-  firstNameDirty: () => TextField;
-  onPasswordChange: (event: ChangeEventType) => TextField;
-  clearPassword: () => TextField;
-  passwordDirty: () => TextField;
-  onConfirmPasswordChange: (event: ChangeEventType) => TextField;
-  clearConfirmPassword: () => TextField;
-  confirmPasswordDirty: () => TextField;
 }
 
-interface RegisterFormErrors {
+interface RegisterFormErrors extends ProfileFormErrors {
   emailError: (email: TextField) => boolean;
   phoneError: (phone: TextField) => boolean;
-  lastNameError: (lastName: TextField) => boolean;
-  firstNameError: (firstName: TextField) => boolean;
-  passwordError: (password: TextField) => boolean;
-  confirmPasswordError: (confirmPassword: TextField) => boolean;
 }
 
 interface RegisterFormSubmit {
