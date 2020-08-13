f25403ecfac392300528af73157beea102f42ede
Andrey Mashukov <andre.mashukov@gmail.com>
2020-08-13 18:30:49 +0800

    Orders table layout

--- src/pages/admin/orders.tsx
+++ src/pages/admin/orders.tsx
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import '../Layout.scss';
 import { AdminBreadcrumbs } from '../../components';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const AdminOrdersPage = () => {
+  const [grid] = useState({
+    columnDefs: [
+      {
+        headerName: 'ID',
+        field: 'id',
+        checkboxSelection: true
+      },
+      {
+        headerName: 'Регион',
+        field: 'region'
+      },
+      {
+        headerName: 'Адрес доставки',
+        field: 'address'
+      },
+      {
+        headerName: 'Сумма',
+        field: 'price'
+      },
+      {
+        headerName: 'Покупатель',
+        field: 'buyer'
+      }
+    ],
+    rowData: [
+      {
+        id: 'Toyota',
+        region: 'Celica',
+        address: 'Address',
+        price: 35000
+      },
+      {
+        id: 'Ford',
+        region: 'Mondeo',
+        address: 'Address',
+        price: 32000
+      },
+      {
+        id: 'Porsche',
+        region: 'Boxter',
+        address: 'Address',
+        price: 72000
+      }
+    ],
+    defaultColDef: {
+      sortable: true,
+      resizable: true,
+      flex: 2,
+      minWidth: 150
+    },
+    rowSelection: 'multiple'
+  });
   return (
-    <div className="page-root-layout">
+    <div className="page-root-admin-layout">
       <Grid
         direction="column"
         justify="flex-start"
@@ -26,6 +81,22 @@ const AdminOrdersPage = () => {
         </Grid>
         <Grid item>
           <AdminBreadcrumbs name="Заказы" />
+        </Grid>
+        <Grid item>
+          <div
+            className="ag-theme-alpine"
+            style={{
+              height: '70vh',
+              width: '80vw',
+              padding: '10px'
+            }}
+          >
+            <AgGridReact
+              columnDefs={grid.columnDefs}
+              rowData={grid.rowData}
+              defaultColDef={grid.defaultColDef}
+            ></AgGridReact>
+          </div>
         </Grid>
       </Grid>
     </div>
