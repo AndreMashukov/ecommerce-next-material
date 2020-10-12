195ed519f830fa7b2224ae6755f793b05db9f3d4
Andrey Mashukov <andre.mashukov@gmail.com>
2020-10-12 15:25:07 +0800

    minor changes

--- next.config.js
+++ next.config.js
@@ -2,19 +2,25 @@ const withTypescript = require('@zeit/next-typescript');
 module.exports = withTypescript();
 
 const withSass = require('@zeit/next-sass');
-const withCSS = require("@zeit/next-css");
-module.exports = withCSS(withSass({
-   webpack (config, options) {
-       config.module.rules.push({
-           test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-           use: {
-               loader: 'url-loader',
-               options: {
-                   limit: 100000
-               }
-           }
-       });
+const withCSS = require('@zeit/next-css');
+module.exports = withCSS(
+  withSass({
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000
+          }
+        }
+      });
+      config.resolve = {
+        ...config.resolve, // this line will fixed
+        modules: ['node_modules', './src']
+      };
 
-       return config;
-   }
-}));
+      return config;
+    }
+  })
+);
