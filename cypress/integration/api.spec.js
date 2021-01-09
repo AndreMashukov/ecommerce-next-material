dae7e77b3073e031817e6b0c896146f90770ba39
Andrey Mashukov <andre.mashukov@gmail.com>
2021-01-09 12:05:29 +0800

    Test for cookies

--- cypress/integration/api.spec.js
+++ cypress/integration/api.spec.js
@@ -3,11 +3,9 @@
 /// <reference types="Cypress" />
 
 describe('Ecommerce Frontend', () => {
-  it('API works', () => {
+  it('API works', async () => {
     const api = require('../../global-definitions.json').dev.baseUrl;
-    cy.request('GET', api).then((response) => {
-      // response.body is automatically serialized into JSON
-      expect(response.body).to.have.property('name', 'ecommerce-backend');
-    });
+    const response = await cy.request('GET', api);
+    expect(response.body).to.have.property('name', 'ecommerce-backend');
   });
 });
---
+++ cypress/integration/cookies.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+context('Cookies', () => {
+  beforeEach(() => {
+    Cypress.Cookies.debug(true);
+
+    cy.visit('/');
+    cy.clearCookies();
+  });
+
+  it('get a sessionId from browser cookie', () => {
+    cy.visit('/');
+    cy.wait(2000);
+    // cy.getCookie() yields a cookie object
+    cy.getCookies()
+      .should('have.length', 1)
+      .should((cookies) => {
+        expect(cookies[0].name).to.be.equal('sessionId');
+      });
+  });
+});
