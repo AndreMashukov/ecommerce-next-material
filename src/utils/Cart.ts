f254fe52c137dbecdcc7ea800cf95782bd751f80
Andrey Mashukov <andre.mashukov@gmail.com>
2021-04-04 15:29:33 +0800

    carry~lin
    SC can write data in two places.
        1. Global state.
            state variables in a given SC.
            are stored in ETH global state trie.
            SC cannot read or write data
            to other SC.
        2. Logs.
            SC unable to read the on-chain
            log events they've created.

--- src/utils/Cart.ts
+++ src/utils/Cart.ts
@@ -1,18 +1,18 @@
 import { CartItem } from '../models';
 
 export const getCartItemsNumber = (cart: CartItem[]) => {
-  return cart.reduce((itemsNumber, item) => {
+  return cart.length > 0 ? cart.reduce((itemsNumber, item) => {
     itemsNumber += item.quantity;
 
     return itemsNumber;
-  }, 0);
+  }, 0): 0;
 };
 
 
 export const getCartTotal = (cart: CartItem[]) => {
-  return cart.reduce((total, item) => {
+  return cart.length > 0 ? cart.reduce((total, item) => {
     total += item.quantity * item.price;
 
     return total;
-  }, 0);
+  }, 0) : 0;
 };
