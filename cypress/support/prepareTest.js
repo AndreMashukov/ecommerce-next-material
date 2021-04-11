b867eff8ed08dcc5f0449cf1c9e343b05f585aba
Andrey Mashukov <andre.mashukov@gmail.com>
2021-04-11 15:28:01 +0800

    yihn sik.
    -.1.
    to know someone.

--- cypress/support/prepareTest.js
+++ cypress/support/prepareTest.js
@@ -1,5 +1,15 @@
 export const prepareTest = () => {
+  const api = require('../../global-definitions.json').dev.baseUrl;
+  const prepareTestEndpoint = `${api}/personal/orders/test/prepare`
   let userState = JSON.parse(localStorage.getItem('userState'));
+
   cy.log('user id', userState.id);
   expect(userState.id.length).to.eq(36);
+
+  cy.request('POST', prepareTestEndpoint, {
+    id: userState.id
+  }).then(response => {
+    expect(response.status).to.eq(200);
+    cy.log('prepareTest response', response.body);
+  });
 }
